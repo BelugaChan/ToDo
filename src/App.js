1690429e8ae547a5da18cb4f6aae4bab2b00e2fc
BelugaChan <113057002+BelugaChan@users.noreply.github.com>
2023-06-23 15:47:52 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return <div className="todo">
+    <div className="todo_sidebar">
+      <button>Hello</button>
+    </div>
+    <div className="todo_tasks">123</div>
+  </div>;
+}
+export default App;
